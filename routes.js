ffc516fbc3876679b4ad807422ce0d32af479a26
Pato Pitaluga <patricio.pitaluga@gmail.com>
2021-04-13 01:31:30 -0300

    Fix

--- routes.js
+++ routes.js
@@ -4,5 +4,6 @@
  * @param {object} _app - the express instance.
  */
 module.exports = (_app) => {
+  _app.get('/', (_req, _res) => { _res.send('Api running.') });
   _app.get('/convert-string', require('./controllers/ctrl-convert-string'));
 }
